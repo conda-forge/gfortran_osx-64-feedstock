ea092aebb89a18f1e7d2b8d2b0ebf7c8495b80f4
David Brochart <david.brochart@gmail.com>
2019-12-27 00:24:04 +0100

    -

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -19,8 +19,8 @@ $FC -O3 -fopenmp -ffast-math -o maths maths.f90 -v
 rm -f maths
 
 $FC -c print_hello.f90 -o print_hello.o
-clang++ -c main.cpp -o main.o
-clang++ main.o print_hello.o -o main -lgfortran
+${CXX} -c main.cpp -o main.o
+${CXX} main.o print_hello.o -o main ${LDFLAGS} -lgfortran
 rm print_hello.o main.o main
 
 if [[ ! $FFLAGS ]]
