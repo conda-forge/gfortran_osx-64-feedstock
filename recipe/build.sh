f45ffe6935dcb5685f550c128a19dd7bc56a84e9
Isuru Fernando <isuruf@gmail.com>
2020-09-07 11:32:03 -0500

    No need of compilers in host

--- recipe/build.sh
+++ recipe/build.sh
@@ -27,10 +27,6 @@ mkdir -p ${PREFIX}/etc/conda/{de,}activate.d
 cp "${SRC_DIR}"/activate-gfortran.sh ${PREFIX}/etc/conda/activate.d/activate-${PKG_NAME}.sh
 cp "${SRC_DIR}"/deactivate-gfortran.sh ${PREFIX}/etc/conda/deactivate.d/deactivate-${PKG_NAME}.sh
 
-# Stop conda-build from following links
-rm ${PREFIX}/bin/clang
-touch ${PREFIX}/bin/clang
-
 ln -s ${PREFIX}/bin/${CHOST}-ar       $PREFIX/lib/gcc/${CHOST}/${gfortran_version}/ar
 ln -s ${PREFIX}/bin/${CHOST}-as       $PREFIX/lib/gcc/${CHOST}/${gfortran_version}/as
 ln -s ${PREFIX}/bin/clang             $PREFIX/lib/gcc/${CHOST}/${gfortran_version}/clang
